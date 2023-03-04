70a6c105afa894312f996e044621b3b3afd5f61d
MartinCespedes <martin.cespedes002@mymdc.net>
2023-03-04 08:21:10 -0500

    Added .json files with dependencies & added server.js file

--- db/cconnection.js
+++ db/cconnection.js
@@ -1,11 +1,20 @@
-const mysql = require("mysql2");
+const mysql = require("mysql");
 
-// Connect to database
-const db = mysql.createConnection({
+// Create a connection to the database
+const connection = mysql.createConnection({
   host: "localhost",
   user: "root",
   password: "root",
-  database: "",
+  database: "employee_tracker_db",
 });
 
-module.exports = db;
+// Connect to the database
+connection.connect((err) => {
+  if (err) throw err;
+  console.log("Connected to the database.");
+});
+
+// Perform database operations here
+
+// Close the connection when done
+connection.end();
